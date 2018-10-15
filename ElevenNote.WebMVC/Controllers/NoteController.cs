fc1ebfd953a28198f8000aef2c03d23bef59425c
tonyr <tonyr@knowledgeservices.com>
2018-10-15 10:48:07 -0400

    Changes 4.3

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ElevenNote.Models;
 
 namespace ElevenNote.WebMVC.Controllers
 {
+    [Authorize]
     public class NoteController : Controller
     {
         // GET: Note
         public ActionResult Index()
         {
-            return View();
+            var model = new NoteListItem[0];
+            return View(model);
         }
     }
 }
