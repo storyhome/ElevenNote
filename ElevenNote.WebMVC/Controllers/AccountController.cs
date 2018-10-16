570671afd964fa4b4845360af202681a6164cfc9
tonyr <tonyr@knowledgeservices.com>
2018-10-16 12:03:46 -0400

    Add API upto part-16

--- ElevenNote.WebMVC/Controllers/AccountController.cs
+++ ElevenNote.WebMVC/Controllers/AccountController.cs
@@ -13,6 +13,10 @@ using ElevenNote.WebMVC.Models;
 
 namespace ElevenNote.WebMVC.Controllers
 {
+#if !DEBUG
+    [RequireHttps]
+#endif
+
     [Authorize]
     public class AccountController : Controller
     {
@@ -424,7 +428,7 @@ namespace ElevenNote.WebMVC.Controllers
             base.Dispose(disposing);
         }
 
-        #region Helpers
+#region Helpers
         // Used for XSRF protection when adding external logins
         private const string XsrfKey = "XsrfId";
 
@@ -481,6 +485,6 @@ namespace ElevenNote.WebMVC.Controllers
                 context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
             }
         }
-        #endregion
+#endregion
     }
 }
